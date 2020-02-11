2d275d59ab09b967c9fa7f025b39916895d938be
nicovernot <nicovernot@gmail.com>
2020-02-11 07:09:32 +0100

    react2 first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import React from 'react';
+//import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import './Contact'
 import Contact from './Contact';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      error: null,
+      isLoaded: false,
+      items: []
+     }
+  }
 
-    return (
-        <div className="Contact">
-        <Contact nom={nom} url={url} online={online}></Contact>
-        </div>
-    );
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=5")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          this.setState({
+            isLoaded: true,
+            items: result.results
+          });
+        },
+        // Remarque : il est important de traiter les erreurs ici
+        // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+        // des exceptions provenant de réels bugs du composant.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() { 
+    return ( 
+      <div className="App">
+    
+      <ul className="status">
+          {this.state.items.map(item => (
+            <li className={online ? "status-online":"status-offline"} key={item.name.first}>
+              <Contact nom={item.name.first} url={item.picture.large} online={online}></Contact>
+            </li>
+          ))}
+        </ul>
+      </div>
+     );
+  }
 }
+ 
+export default App;
+
+
 
 
 const nom="toto"
 const url="https://placekitten.com/g/64/64"
-const online="true"
+const online="false"
 
-export default App;
--- src/Contact.js
+++ src/Contact.js
@@ -4,16 +4,22 @@ import './App.css';
 class Contact extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+         
+         }
     }
+
+   
+
     render() { 
         return (
- <div>
-Nom: <p className="name">{this.props.nom}</p>
-<div className={this.props.online ? "status status-online":"status status-ofline"}>
-En ligne :<p className="status-text">{this.props.online ? 'est actuellement en ligne' : 'n’est pas en ligne'}</p>
-</div>
+ <div className="Contact" >
+<p className="name">{this.props.nom}</p>
+<br></br>
 <img className="avatar" src={this.props.url}/>
+<div className={this.props.online ? "status status-online":"status status-ofline"}>
+<p className="status-text">{this.props.online ? 'en ligne' : 'n’est pas en ligne'}</p>
+</div>
 
  </div>
 
